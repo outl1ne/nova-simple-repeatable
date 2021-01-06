701ac5a1d826cd4d7d729cb7075bb4b79b83be2c
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-01-06 17:21:28 +0200

    Add detail field

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,101 @@
+<template>
+  <panel-item :field="field">
+    <template slot="value">
+      <div class="overflow-hidden relative rounded-lg bg-white shadow border border-60" v-if="values">
+        <table class="table w-full table-default nova-resource-table">
+          <thead>
+            <tr>
+              <th v-for="(header, i) in headers" :key="i">{{ header.name }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(row, i) of values" :key="i" class="nova-resource-table-row">
+              <td class="font-mono text-sm" style="height: 2rem" v-for="(value, i) in row" :key="i">{{ value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div v-else>{{ (value && value.label) || '—' }}</div>
+    </template>
+  </panel-item>
+</template>
+
+<script>
+export default {
+  props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+  computed: {
+    values() {
+      const headers = this.headers;
+
+      let value = this.field.value;
+      if (!value) return void 0;
+
+      if (Array.isArray(value)) return this.handleValue(value);
+
+      try {
+        value = JSON.parse(value);
+      } catch (e) {
+        return void 0;
+      }
+      if (!Array.isArray(value)) return void 0;
+
+      return this.handleValue(value);
+    },
+
+    headers() {
+      const fields = this.field.repeatableFields;
+      return fields.map(field => ({ name: field.name, attribute: field.attribute }));
+    },
+  },
+
+  methods: {
+    handleValue(valuesArray) {
+      const fields = this.field.repeatableFields;
+
+      const fieldsWithOptions = fields.filter(field => Array.isArray(field.options) && !!field.options.length);
+      if (fieldsWithOptions.length > 0) {
+        fieldsWithOptions.forEach(field => {
+          const attribute = field.attribute;
+          valuesArray = valuesArray.map(entry => {
+            const id = entry[field.attribute];
+
+            if (!!id) {
+              // Try the usual label-value pair
+              if (field.options[0].value && field.options[0].label) {
+                // Yay, we recognize this format
+                const optionMatch = field.options.find(o => String(o.value) === String(id));
+                entry[field.attribute] = optionMatch ? optionMatch.label : id;
+              }
+
+              // Also try the key-value pair format
+              else if (!!field.options[id]) {
+                entry[field.attribute] = field.options[id];
+              }
+            }
+            return entry;
+          });
+        });
+      }
+
+      return this.sortValueByHeaders(valuesArray);
+    },
+
+    sortValueByHeaders(valuesArray) {
+      const headers = this.headers;
+
+      const newArray = [];
+      valuesArray.forEach(entry => {
+        const temp = [];
+        headers.forEach(header => {
+          temp.push(entry[header.attribute]);
+        });
+        newArray.push(temp);
+      });
+
+      return newArray;
+    },
+  },
+};
+</script>
--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,7 @@
 import FormField from './components/FormField';
+import DetailField from './components/DetailField';
 
 Nova.booting((Vue, router) => {
   Vue.component('form-simple-repeatable', FormField);
+  Vue.component('detail-simple-repeatable', DetailField);
 });
