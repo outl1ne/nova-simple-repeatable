380007c78e12eae6d466393c22c68e58f7546020
Marttin Notta <marttin@outl1ne.com>
2023-07-03 15:31:29 +0300

    Fix locale sorting when populating list

--- resources/js/mixins/HandlesRepeatable.js
+++ resources/js/mixins/HandlesRepeatable.js
@@ -68,8 +68,8 @@ export default {
 
       if (field.translatable.prioritize_nova_locale) {
         localeKeys = localeKeys.sort((a, b) => {
-          if (a === Nova.config.locale && b !== Nova.config.locale) return -1;
-          if (a !== Nova.config.locale && b === Nova.config.locale) return 1;
+          if (a === Nova.config('locale') && b !== Nova.config('locale')) return -1;
+          if (a !== Nova.config('locale') && b === Nova.config('locale')) return 1;
           return 0;
         });
       }
